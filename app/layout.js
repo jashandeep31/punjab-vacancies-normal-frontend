3ae0bd6e411ba8052ec190dcd8e56862c6b9c4c4
Jashandeep Singh <jashandeep1659@gmail.com>
2023-07-07 21:33:39 +0530

    0.0.1

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5294151828912060"
+                    crossorigin="anonymous"
+                ></script>
+            </head>
             <body className={inter.className}>
                 <UserState>{children}</UserState>
             </body>
