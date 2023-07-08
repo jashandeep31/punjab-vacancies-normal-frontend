0134668b5470b09efbfb8ec1b9670483f904828b
Jashandeep Singh <jashandeep1659@gmail.com>
2023-07-08 17:19:43 +0530

    added options in the create

--- helpers/axiosInstance.js
+++ helpers/axiosInstance.js
@@ -1,2 +1,3 @@
-// export const BaseURL = "https://api.punjabvacancies.live/";
-export const BaseURL = "http://localhost:8000/";
+export const BaseURL = "https://api.punjabvacancies.live/";
+
+// export const BaseURL = "http://localhost:8000/";
