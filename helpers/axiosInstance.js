7aaa711c61bf2c318da20de63bd020b322a9fade
Jashandeep Singh <jashandeep1659@gmail.com>
2023-07-13 21:46:05 +0530

    url updated

--- helpers/axiosInstance.js
+++ helpers/axiosInstance.js
@@ -1,2 +1,2 @@
-// export const BaseURL = "https://api.punjabvacancies.live/";
-export const BaseURL = "http://localhost:8000/";
+export const BaseURL = "https://api.punjabvacancies.live/";
+// export const BaseURL = "http://localhost:8000/";
