5870e86893b344559681ca55f927b9b8fe1d7ba0
Jashandeep Singh <jashandeep1659@gmail.com>
2023-08-02 21:17:53 +0530

    back to prev ads

--- app/layout.client.js
+++ app/layout.client.js
@@ -1,29 +1,15 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-import Script from "next/script";
-const LayoutClient = () => {
-    const [adKey, setAdKey] = useState(0);
 
-    useEffect(() => {
-        // Increment the adKey whenever the page changes
-        setAdKey(adKey + 1);
-    }, []);
+const LayoutClient = () => {
     return (
-        <>
-            <ProgressBar
-                height="4px"
-                color="#E85646"
-                options={{ showSpinner: false }}
-                shallowRouting
-            />
-            <Script
-                async
-                key={adKey}
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5294151828912060"
-                crossOrigin="anonymous"
-            ></Script>
-        </>
+        <ProgressBar
+            height="4px"
+            color="#E85646"
+            options={{ showSpinner: false }}
+            shallowRouting
+        />
     );
 };
 
